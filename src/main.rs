f938b908bde421dfc27c4b5c50e3d56efed24188
Ilya Kramer <ilya@lightricks.com>
2021-05-09 20:53:42 +0300

    Pre-cv-fix

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, BTreeMap};
 use std::error::Error;
 use std::fmt::Display;
 use std::io::Write;
@@ -22,7 +22,7 @@ impl<W: PartialOrd + Clone + Add<Output=W> + Display> DVValue<W> {
         }
     }
 
-    fn write_html_long(&self, names: &HashMap<usize, String>) -> String {
+    fn write_html_long(&self, names: &BTreeMap<usize, String>) -> String {
         match self {
             DVValue::Infinity => String::from("&infin;"),
             DVValue::Distance(v, id) => format!(
@@ -52,7 +52,7 @@ enum Operation<W: PartialOrd + Clone + Add<Output=W> + Display> {
 #[derive(Debug)]
 struct World<W: PartialOrd + Clone + Add<Output=W> + Display> {
     nodes: HashMap<String, Node<W>>,
-    node_names: HashMap<usize, String>,
+    node_names: BTreeMap<usize, String>,
     generation: u32,
 }
 
@@ -86,7 +86,7 @@ fn modify_dv<W: PartialOrd + Clone + Add<Output=W> + Display>(
 impl<W: PartialOrd + Clone + Add<Output=W> + Display> World<W> {
     pub fn new(node_names: Vec<&str>) -> World<W> {
         let mut result: HashMap<String, Node<W>> = HashMap::with_capacity(node_names.len());
-        let mut node_names_out: HashMap<usize, String> = HashMap::new();
+        let mut node_names_out: BTreeMap<usize, String> = BTreeMap::new();
         let mut index: usize = 0;
 
         for name in &node_names {
@@ -133,28 +133,41 @@ impl<W: PartialOrd + Clone + Add<Output=W> + Display> World<W> {
         ))
     }
 
-    fn print_change<Writer: Write>(&self, writer: &mut Writer, node: &Node<W>, new_dv: &Vec<DVValue<W>>) -> Result<(), Box<dyn Error>> {
+    fn print_node<Writer: Write>(&self, writer: &mut Writer, node: &Node<W>, changed: Option<&Vec<DVValue<W>>>) -> Result<(), Box<dyn Error>> {
         writeln!(writer, "<table>\n\t<tr>")?;
         writeln!(writer, "\t\t<th>{}</th>", node.name)?;
-        for (index, _) in new_dv.iter().enumerate() {
-            writeln!(writer, "\t\t<th>{}</th>", self.node_names.get(&index).unwrap())?;
+
+        for node_name in self.node_names.values() {
+            writeln!(writer, "\t\t<th>{}</th>", node_name)?;
         }
+
         writeln!(writer, "\t</tr>\n\t<tr>\n\t\t<th>{}</th>",node.name)?;
-        for (index, new_value) in new_dv.iter().enumerate() {
-            if new_value == node.dv.get(index).unwrap() {
+        // If there's a new dv, run the more complex algorithm
+        if let Some(new_dv) = changed {
+            for (index, new_value) in new_dv.iter().enumerate() {
+                if new_value == node.dv.get(index).unwrap() {
+                    writeln!(writer, "\t\t<td>{}</td>", new_value.write_html_long(&self.node_names))?;
+                } else {
+                    writeln!(
+                        writer,
+                        "\t\t<td>{}&#8594;{}</td>",
+                        node.dv.get(index).unwrap().write_html_long(&self.node_names),
+                        new_value.write_html_long(&self.node_names)
+                    )?;
+                }
+            }
+        } else {
+            for new_value in &node.dv {
                 writeln!(writer, "\t\t<td>{}</td>", new_value.write_html_long(&self.node_names))?;
-            } else {
-                writeln!(
-                    writer,
-                    "\t\t<td>{}&#8594;{}</td>",
-                    node.dv.get(index).unwrap().write_html_long(&self.node_names),
-                    new_value.write_html_long(&self.node_names)
-                )?;
             }
         }
+
         writeln!(writer, "\t</tr>")?;
 
-        for (index, inbox) in &node.inbox {
+        let mut inbox_sorted = node.inbox.clone();
+        inbox_sorted.sort_by_key(|pair| pair.0);
+
+        for (index, inbox) in &inbox_sorted {
             writeln!(
                 writer,
                 "\t<tr>\n\t\t<th>{}</th>",
@@ -174,7 +187,6 @@ impl<W: PartialOrd + Clone + Add<Output=W> + Display> World<W> {
     }
 
     fn apply_operations(&self, operations: Vec<Operation<W>>) -> Self {
-        let mut changed_nodes: HashSet<usize> = HashSet::new();
         let mut new_state: HashMap<String, Node<W>> = HashMap::new();
 
         let mut relations: HashMap<(usize, usize), W> = HashMap::new();
@@ -217,9 +229,6 @@ impl<W: PartialOrd + Clone + Add<Output=W> + Display> World<W> {
                             new_w.to_owned(),
                         ),
                     );
-
-                    changed_nodes.insert(node_a);
-                    changed_nodes.insert(node_b);
                 }
             }
         }
@@ -230,10 +239,7 @@ impl<W: PartialOrd + Clone + Add<Output=W> + Display> World<W> {
 
             for ((node_a, node_b), new_w) in &relations {
                 if *node_a == node.index {
-                    if changed_nodes.contains(node_b) {
-                        inbox.push((*node_b, new_dvs.get(node_b).unwrap().to_owned()));
-                    }
-
+                    inbox.push((*node_b, new_dvs.get(node_b).unwrap().to_owned()));
                     connected.push((*node_b, new_w.to_owned()))
                 }
             }
@@ -250,8 +256,33 @@ impl<W: PartialOrd + Clone + Add<Output=W> + Display> World<W> {
         World { nodes: new_state, generation: self.generation, node_names: self.node_names.to_owned() }
     }
 
-    fn run_simulation<Writer: Write>(&self, writer: &mut Writer) -> Result<NewState<W>, Box<dyn Error>> {
+    fn sorted_nodes(&self) -> Vec<&Node<W>> {
+        let mut indexes:Vec<&usize> = self.node_names.keys().collect();
+        indexes.sort();
+
+        let mut nodes:Vec<&Node<W>> = Vec::new();
+
+        for index in indexes {
+            let node_name = self.node_names.get(index).unwrap();
+
+            nodes.push(self.nodes.get(node_name).unwrap());
+        }
+
+        nodes
+    }
+
+    fn print_state<Writer: Write>(&self, writer: &mut Writer) -> Result<(), Box<dyn Error>> {
         writeln!(writer, "<h2>t={}</h2>", self.generation)?;
+
+        for node in self.sorted_nodes() {
+            self.print_node(writer, node, None)?;
+        }
+
+        Ok(())
+    }
+
+    fn run_simulation<Writer: Write>(&self, writer: &mut Writer) -> Result<NewState<W>, Box<dyn Error>> {
+        writeln!(writer, "<h2>t={}</h2>", self.generation + 1)?;
 
         let mut changed_nodes: HashSet<usize> = HashSet::new();
 
@@ -261,17 +292,17 @@ impl<W: PartialOrd + Clone + Add<Output=W> + Display> World<W> {
         let mut indexes:Vec<&usize> = self.node_names.keys().collect();
         indexes.sort();
 
-        for index in indexes {
-            let node = self
-                .nodes
-                .get(self.node_names.get(index).unwrap())
-                .unwrap();
-
+        for node in self.sorted_nodes() {
             let mut direct_cost: HashMap<usize, W> = HashMap::new();
 
             for (conn, v) in &node.connected {
                 relations.insert((node.index, conn.to_owned()), v.to_owned());
-                direct_cost.insert(conn.to_owned(), v.to_owned());
+                //direct_cost.insert(conn.to_owned(), v.to_owned());
+                let cost:Option<W> = match node.dv.get(*conn).unwrap()  {
+                    DVValue::Distance(w, _) => Some(w.to_owned()),
+                    _ => None
+                };
+                direct_cost.insert(conn.to_owned(), cost.unwrap());
             }
 
             let mut new_dv = Vec::new();
@@ -279,25 +310,31 @@ impl<W: PartialOrd + Clone + Add<Output=W> + Display> World<W> {
 
             let mut lines: Vec<String> = Vec::new();
 
-            for v in &node.dv {
+            for v_old in &node.dv {
                 if index == node.index {
-                    lines.push(format!("D<sub>{}</sub>({})=0", node.name, node.name));
+                    // lines.push(format!("D<sub>{}</sub>({})=0", node.name, node.name));
                     new_dv.push(DVValue::SameNode);
                 } else {
-                    let mut v = v.clone();
+                    let mut v:DVValue<W> = DVValue::Infinity;
 
                     // For debug printing
                     let mut line = format!(
-                        "d<sub>{}</sub>({})=min(C({},{})",
-                        node.name,
-                        self.node_names.get(&index).unwrap(),
+                        "d<sub>{}</sub>({})=min(",
                         node.name,
                         self.node_names.get(&index).unwrap()
                     );
 
+                    let mut first = true;
+
                     for (node_b, _) in &node.inbox {
+                        if first {
+                            first = false;
+                        } else {
+                            line += ", ";
+                        }
+
                         line += format!(
-                            ",C({},{})+d<sub>{}</sub>({})",
+                            "C({},{})+d<sub>{}</sub>({})",
                             node.name,
                             self.node_names.get(node_b).unwrap(),
                             self.node_names.get(node_b).unwrap(),
@@ -306,10 +343,15 @@ impl<W: PartialOrd + Clone + Add<Output=W> + Display> World<W> {
                     }
 
                     line += ")=min(";
-                    line += v.write_html().as_str();
+                    let mut first = true;
 
                     for (node_b, node_b_vector) in &node.inbox {
-                        line += ",";
+                        if first {
+                            first = false;
+                        } else {
+                            line += ", ";
+                        }
+
                         line += format!("{}", direct_cost.get(node_b).unwrap()).as_str();
                         line += "+";
                         line += node_b_vector.get(index).unwrap().write_html().as_str();
@@ -332,7 +374,6 @@ impl<W: PartialOrd + Clone + Add<Output=W> + Display> World<W> {
 
                             if replace {
                                 v = DVValue::Distance(new_w, *node_b);
-                                changed_nodes.insert(node.index);
                             }
                         }
                     }
@@ -340,22 +381,23 @@ impl<W: PartialOrd + Clone + Add<Output=W> + Display> World<W> {
                     line += ")=";
                     line += v.write_html().as_str();
 
+                    if v != v_old.to_owned() {
+                        changed_nodes.insert(node.index);
+                    }
+
                     new_dv.push(v);
+
                     lines.push(line);
                 }
 
                 index += 1;
             }
 
-            writeln!(writer, "<div class=\"single_table\">")?;
-            self.print_change(writer, node, &new_dv)?;
-            writeln!(writer, "\t<div class=\"details\">")?;
+            self.print_node(writer, node, Some(&new_dv))?;
+            writeln!(writer, "<div class=\"details\">")?;
             for line in lines {
-                writeln!(writer, "\t\t<div>{}</div>", line)?;
+                writeln!(writer, "\t<div>{}</div>", line)?;
             }
-            writeln!(writer, "\t</div>")?;
-
-
             writeln!(writer, "</div>")?;
 
             new_dvs.insert(node.index, new_dv);
@@ -372,10 +414,7 @@ impl<W: PartialOrd + Clone + Add<Output=W> + Display> World<W> {
 
                 for ((node_a, node_b), new_w) in &relations {
                     if *node_a == node.index {
-                        if changed_nodes.contains(node_b) {
-                            inbox.push((*node_b, new_dvs.get(node_b).unwrap().to_owned()));
-                        }
-
+                        inbox.push((*node_b, new_dvs.get(node_b).unwrap().to_owned()));
                         connected.push((*node_b, new_w.to_owned()))
                     }
                 }
@@ -413,15 +452,33 @@ fn initial_world() -> Result<World<u32>, Box<dyn Error>> {
     )))
 }
 
+fn initial_world2() -> Result<World<u32>, Box<dyn Error>> {
+    let world: World<u32> = World::new(vec!("A", "B", "C", "D"));
+    //println!("op:{:#?}", world.add_interface("A","B",12)?);
+
+    Ok(world.apply_operations(vec!(
+        world.add_interface("A", "B", 2)?,
+        world.add_interface("B", "C", 7)?,
+        world.add_interface("C", "D", 4)?,
+        world.add_interface("A", "D", 8)?,
+        world.add_interface("B", "D", 9)?
+    )))
+}
+
 fn run_until_stable<Writer: Write>(writer: &mut Writer, world: World<u32>) -> Result<World<u32>, Box<dyn Error>> {
     match world.run_simulation(writer)? {
         NewState::Changed(w2) => run_until_stable(writer, w2),
-        NewState::NotChanged => Ok(world)
+        // When no-change advance the generation on by 1
+        NewState::NotChanged => Ok(World{
+            node_names: world.node_names,
+            nodes: world.nodes,
+            generation: world.generation+1
+        })
     }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let world: World<u32> = initial_world()?;
+    let world: World<u32> = initial_world2()?;
     let mut file = File::create("/Users/ilya/Desktop/result.html")?;
 
     writeln!(file, "<!DOCTYPE html>")?;
@@ -429,8 +486,20 @@ fn main() -> Result<(), Box<dyn Error>> {
     writeln!(file, "<link rel=\"stylesheet\" href=\"styles.css\">")?;
 
     writeln!(file, "</head>\n<body>")?;
+    writeln!(file, "<div class=\"wrapper\">")?;
 
-    run_until_stable(&mut file, world)?;
+    world.print_state(&mut file)?;
+    let world1_done = run_until_stable(&mut file, world)?;
+
+    let world2 = world1_done.apply_operations(vec!(
+        world1_done.add_interface("B", "D", 80)?
+    ));
+
+    world2.print_state(&mut file)?;
+
+    run_until_stable(&mut file, world2)?;
+
+    writeln!(file, "</div>")?;
     writeln!(file, "</body>\n</html>")?;
 
     Ok(())
